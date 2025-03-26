f818b5a07abdd3dad6abc1cc52288eef4e0fa5bc
CaramelF <letshegosephiri@gmail.com>
2025-03-26 23:44:19 +0200

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,11 +1,31 @@
+# empty list
 my_list = []
+
+# Append elements to my_list
+ 
 my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 my_list.append(40)
+
+# Insert the value 15 at the second position
+
 my_list.insert(1,15)
-my_list.extend([50, 60, 70])
-del my_list[-1]
+
+# Extend my_list with another list
+
+ur_list =[50, 60, 70]
+my_list.extend(ur_list)
+
+# remove the last element from my_list
+
+#del my_list[7] 
+my_list.pop( )
+
+# Sort my_list in ascending order
+
 my_list.sort()
-index_of_30 = my_list.index(30)
-print(index_of_30)
+
+#Find and print the index of the value 30
+index = my_list.index(30)
+print(index)
